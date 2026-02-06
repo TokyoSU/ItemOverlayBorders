e35740fa268ae20fc0f81468673c93480d3aa14e
TokyoSU <77746747+TokyoSU@users.noreply.github.com>
2026-02-06 12:56:26 +0100

    Made it compatible with my CashShopMod.

---
+++ src/main/java/net/tokyosu/itemoverlayborder/mixin/InvBuilderMixin.java
@@ -0,0 +1,22 @@
+package net.tokyosu.itemoverlayborder.mixin;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.world.item.ItemStack;
+import net.tokyosu.cashshop.utils.InvBuilder;
+import net.tokyosu.itemoverlayborder.client.BorderRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = InvBuilder.class, remap = false)
+public class InvBuilderMixin {
+    @Shadow
+    private GuiGraphics pGui;
+
+    @Inject(method = "drawIcon", at = @At("RETURN"))
+    public void drawIcon(ItemStack stack, int x, int y, CallbackInfo ci) {
+        BorderRenderer.render(this.pGui, x, y, stack);
+    }
+}
